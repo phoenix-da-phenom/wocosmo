cb01f2176e7ea985f41c1e70490fcaac2ec4b741
phoenix-da-phenom <ehinormichael@ymail.com>
2026-03-27 07:37:36 +0100

    Added Naviagtion bar (desktop view only)

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,26 +1,49 @@
 import Image from "next/image";
-import Logo from '../assets/logo.png'
+import Logo from "../assets/logo.png";
+import { FiSearch } from "react-icons/fi";
+import { HiOutlineShoppingCart } from "react-icons/hi";
+import { LuCircleUserRound } from "react-icons/lu";
 
-export default function Navbar (){
+export default function Navbar() {
+  return (
+    <div className="container mx-auto flex items-center justify-between py-4">
 
-    return(
-        <div className="container flex">
-           <div><Image src={Logo} height={40} width={120} alt="logo"/> </div>
-            <div className="flex items-center">
-                <ul className="text-[var(--text-large)] flex gap-10">
-                    <li>Shop</li>
-                    <li>On Sale</li>
-                    <li>New Arrivals</li>
-                    <li>Brands</li>
-                </ul>
-            </div>
-            <div>
-                <input />
-            </div>
+      {/* LEFT: Logo */}
+      <div>
+        <Image src={Logo} height={40} width={120} alt="logo" />
+      </div>
 
+      {/* MIDDLE: Nav + Search */}
+      <div className="flex items-center gap-6">
+        
+        {/* Navigation */}
+        <ul className="flex items-center gap-8 text-[var(--text-large)]">
+          <li className="cursor-pointer hover:text-gray-500">Shop</li>
+          <li className="cursor-pointer hover:text-gray-500">On Sale</li>
+          <li className="cursor-pointer hover:text-gray-500">New Arrivals</li>
+          <li className="cursor-pointer hover:text-gray-500">Brands</li>
+        </ul>
+
+        {/* Search */}
+        <div className="relative w-[300px]">
+          <FiSearch className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500" />
+
+          <input
+            type="text"
+            name="searchBox"
+            placeholder="Search Product"
+            className="bg-[var(--secondary-background-color)] pl-10 pr-4 py-2 w-full rounded-3xl outline-none"
+          />
         </div>
 
-    )
-            
+      </div>
 
+      {/* RIGHT: Icons */}
+      <div className="flex items-center gap-4 text-xl">
+        <HiOutlineShoppingCart className="cursor-pointer" />
+        <LuCircleUserRound className="cursor-pointer" />
+      </div>
+
+    </div>
+  );
 }
