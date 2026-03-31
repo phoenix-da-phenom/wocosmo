b8696fce20f58c617f25197dd52ae61da5901e4a
phoenix-da-phenom <ehinormichael@ymail.com>
2026-03-31 11:08:18 +0100

    Back favicon moved to public directory

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    '192.168.56.1',           // your specific IP
+    '192.168.56.1:3000',      // with port (sometimes needed)
+    // Add these if you also use localhost or other hosts:
+    // 'localhost',
+    // 'localhost:3000',
+  ],
 };
 
 export default nextConfig;
